7b9676b59ed7b3230423cfdc05a21d6720f06196
Solariser <nitrochemy@gmail.ccom>
2019-05-25 15:16:12 +0100

    Added more statistics and a sprite

--- poke-app/src/components/PokemonCard/index.js
+++ poke-app/src/components/PokemonCard/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Typography, Button } from '@material-ui/core';
+import Image from 'material-ui-image';
 
 class PokemonCard extends Component {
     constructor(props) {
@@ -29,8 +30,63 @@ class PokemonCard extends Component {
         }
     }
 
+    loadTypes() {
+        if (this.state.pokemon &&
+            this.state.pokemon.types &&
+            this.state.pokemon.types.length > 0) {
+            let type_components = [];
+
+            for (let i = 0; i < this.state.pokemon.types.length; i++) {
+                type_components.push(
+                    <Grid item xs={6} key={ 'type#' + i }>
+                        <Typography>{ this.state.pokemon.types[i].type.name }</Typography>
+                    </Grid>
+                );
+            }
+
+            return (
+                <div>
+                    <Typography variant="h5">Types</Typography>
+                    <Grid container>
+                        { type_components }
+                    </Grid>
+                </div>
+            );
+        }
+    }
+
+    loadStatistics() {
+        if (this.state.pokemon &&
+            this.state.pokemon.stats &&
+            this.state.pokemon.stats.length > 0) {
+            let stat_components = [];
+
+            for (let i = 0; i < this.state.pokemon.stats.length; i++) {
+                stat_components.push(
+                    <Grid container key={ 'stat#' + i }>
+                        <Grid item xs={4}>
+                            <Typography>{ this.state.pokemon.stats[i].stat.name }</Typography>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography>{ this.state.pokemon.stats[i].base_stat }</Typography>
+                        </Grid>
+                    </Grid>
+                );
+            }
+
+            return (
+                <div>
+                    <Typography variant="h5">Statistics</Typography>
+                    { stat_components }
+                </div>
+            );
+        }
+    }
+
     render() {
-        if (this.state.choice > 0) {
+        if (this.state.choice > 0 &&
+            this.state.pokemon &&
+            this.state.pokemon.sprites) {
             return (
                 <div>
                     { this.props.addToFavourites ?
@@ -49,10 +105,16 @@ class PokemonCard extends Component {
                         Remove from Favourites
                     </Button>
                     : "" }
-                    <Typography>#{ this.state.pokemon.id } : { this.state.pokemon.name }</Typography>
+                    <Typography variant="h4">#{ this.state.pokemon.id } : { this.state.pokemon.name }</Typography>
+                    <Image
+                        src={ this.state.pokemon.sprites.front_default }
+                    />
+                    { this.loadTypes() }
+                    { this.loadStatistics() }
                 </div>
             );
         }
+
         return (
             <div>
                 <Typography>You have not selected a pokemon.</Typography>
