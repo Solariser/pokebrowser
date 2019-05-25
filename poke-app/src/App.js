2c66a7d05d11c22040edd49a1043cf469b92e826
Solariser <nitrochemy@gmail.ccom>
2019-05-25 12:57:55 +0100

    Added basic api calls to console output

--- poke-app/src/App.js
+++ poke-app/src/App.js
@@ -1,22 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pokemon: [],
+            loaded: false
+        }
+    }
+
+    componentDidMount() {
+        for (let i = 1; i <= 151; i++) {
+            let url = 'https://pokeapi.co/api/v2/pokemon/' + i;
+
+            fetch(url)
+            .then((pokemon) => {
+                return pokemon.json();
+            })
+            .then((pokemondata) => {
+                console.log(pokemondata.name);
+
+                if (this.state.pokemon.length >= 150) {
+                    this.setState(prevState => ({
+                        pokemon: [...prevState.pokemon, pokemondata],
+                        loaded: true
+                    }));
+                } else {
+                    this.setState(prevState => ({
+                        pokemon: [...prevState.pokemon, pokemondata]
+                    }));
+                }
+            })
+        }
+    }
+
+    render() {
+        console.log(this.state);
+
+        if (this.state.loaded) {
+            console.log("Load successful");
+            console.log(this.state);
+        }
+
+        return (
+            <div>
+                <header>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
+
 }
 
 export default App;
---
+++ poke-app/src/components/PokemonCard/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PokemonCard extends Component {
+    render() {
+        return (
+            <div>
+                Pokemon Card Placeholder
+            </div>
+        );
+    }
+
+}
+
+export default PokemonCard;
